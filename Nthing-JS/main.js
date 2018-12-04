01ab2a302f341809fdca73f4e929ad52dbf33f8d
nycko|MyTopFun <nycko@sina.com>
2018-12-04 18:18:04 +0800

    electron和fullcalendar基本使用
    1：electron打包
    2：fullcalendar增加和删除一个event

--- Nthing-JS/main.js
+++ Nthing-JS/main.js
@@ -1,34 +1,15 @@
-const { app, BrowserWindow , Menu, Tray} = require('electron')
-  
+//该文件为electron应用程序入口，启动浏览器后加载index.html
+const electron = require('electron')
+const { app, BrowserWindow } = require('electron')
+
 function createWindow () {   
-// 创建浏览器窗口
-//https://electronjs.org/docs/api/frameless-window
-win = new BrowserWindow({ width: 1600, height: 300 })
+  // 创建浏览器窗口
+  // 日历模块基本是1:1
+  var window_size = 800;
+  win = new BrowserWindow({ width: window_size, height: window_size })
 
-// 然后加载应用的 index.html。
-win.loadFile('index.html')
+  // 然后加载应用的 index.html。
+  win.loadFile('index.html')
 }
 
-function createTray () {
-    let tray = null
-    tray = new Tray('48X48.ico')
-    const contextMenu = Menu.buildFromTemplate([
-      {label: 'Item1', type: 'radio'},
-      {label: 'Item2', type: 'radio'},
-      {label: 'Item3', type: 'radio', checked: true},
-      {label: 'Item4', type: 'radio'}
-    ])
-    tray.setToolTip('This is my application.')
-    tray.setContextMenu(contextMenu)
-
-}
-
-function on_ready () {
-    createTray()
-    createWindow()
-}
-
-app.on('ready', on_ready)
-
-
-////////////////////////////
+app.on('ready', createWindow)
